2067ab7c3908ae755ccf05300ea8cba6e0ef07a0
ryusoo-star <ryusoohyun12@gmail.com>
2025-10-17 11:56:01 +0200

    Fix LCD initialization and changing lines

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <WiFi.h>
 
 // Create the I2C LCD object (address 0x27, 16 columns, 2 rows)
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 LCDDisplay lcdDisplay(lcd);
 
 // Create the AsyncWebServer object on port 80
@@ -25,76 +25,76 @@ const char index_html[] PROGMEM = R"rawliteral(
   <meta name="viewport" content="width=device-width, initial-scale=1">
   </head><body>
   <form action="/get">
-    input1: <input type="text" name="input1">
+    First line: <input type="text" name="input1">
     <input type="submit" value="Submit">
   </form><br>
   <form action="/get">
-    input2: <input type="text" name="input2">
+    Second line: <input type="text" name="input2">
     <input type="submit" value="Submit">
   </form>
 </body></html>)rawliteral";
 
 void notFound(AsyncWebServerRequest *request)
 {
-    request->send(404, "text/plain", "Not found");
+  request->send(404, "text/plain", "Not found");
 }
+
+String input1Message = "";
+String input2Message = "";
 
 void setup()
 {
-    Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED)
-    {
-        Serial.println("WiFi Failed!");
-        return;
-    }
-    Serial.println();
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("WiFi Failed!");
+    return;
+  }
+  Serial.println();
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 
-    lcdDisplay.init(); // Initialize LCD once here
-    lcd.backlight();
+  lcd.init();
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.clear();
 
-    // Send web page with input fields to client
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(200, "text/html", index_html); });
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/html", index_html); });
 
-    // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
-    server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
-              {
-    String inputMessage;
-    String inputParam;
-    if (request->hasParam(PARAM_INPUT_1)) {
-        inputMessage = request->getParam(PARAM_INPUT_1)->value();
-        inputParam = PARAM_INPUT_1;
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+    if (request->hasParam(PARAM_INPUT_1) && request->getParam(PARAM_INPUT_1)->value() != "") {
+        input1Message = request->getParam(PARAM_INPUT_1)->value();
+    //     delay(500);
+    // lcd.setCursor(0, 0); // First line
+    // lcd.print(input1Message);
+    // delay(100);
     }
-    else if (request->hasParam(PARAM_INPUT_2)) {
-        inputMessage = request->getParam(PARAM_INPUT_2)->value();
-        inputParam = PARAM_INPUT_2;
-    }
-    else {
-        inputMessage = "No message sent";
-        inputParam = "none";
-    }
-    Serial.println(inputMessage);
-
-    // Show Input message on LCD
-    if (inputParam == PARAM_INPUT_2) {
-        //lcd.clear();
-        lcd.setCursor(0, 3); // Second line
-        lcd.print(inputParam + ": " + inputMessage);
-    } else {
-        //lcd.clear();
-        lcd.setCursor(0, 1); // First line
-        lcd.print(inputParam + ": " + inputMessage);
+    if (request->hasParam(PARAM_INPUT_2) && request->getParam(PARAM_INPUT_2)->value() != "") {
+        input2Message = request->getParam(PARAM_INPUT_2)->value();
+        lcd.setCursor(0, 1); // Second line
+    lcd.print(input2Message);
+    delay(100);
     }
 
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
-                                     + inputParam + ") with value: " + inputMessage +
-                                     "<br><a href=\"/\">Return to Home Page</a>"); });
-    server.onNotFound(notFound);
-    server.begin();
+    // Show both messages on LCD
+    // delay(500);
+    // lcd.setCursor(0, 0); // First line
+    // lcd.print(input1Message);
+    // delay(100);
+
+    // lcd.setCursor(0, 1); // Second line
+    // lcd.print(input2Message);
+    // delay(100);
+
+    request->send(200, "text/html", "HTTP GET request sent to your ESP<br><a href=\"/\">Return to Home Page</a>"); });
+  server.onNotFound(notFound);
+  server.begin();
 }
 
 void loop()
